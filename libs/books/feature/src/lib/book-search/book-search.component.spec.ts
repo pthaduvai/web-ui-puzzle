493e6a9393c5a601a8eae0666cd29c0b2614ea70
Praneeth Thaduvai <pthaduvai@pkglobal.com>
2020-12-24 11:13:05 -0600

    commiting the changes of the code for code review and refactorin

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
-
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
 
