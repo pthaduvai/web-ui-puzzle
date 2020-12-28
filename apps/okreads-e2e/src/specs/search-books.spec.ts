205271eac859df4ef132b5fbce59dde5bbd7079d
Praneeth Thaduvai <pthaduvai@pkglobal.com>
2020-12-28 11:19:38 -0600

    changes for the instant search

--- apps/okreads-e2e/src/specs/search-books.spec.ts
+++ apps/okreads-e2e/src/specs/search-books.spec.ts
@@ -1,4 +1,5 @@
 import { $, $$, browser, ExpectedConditions } from 'protractor';
+import { expect } from 'chai';
 
 describe('When: Use the search feature', () => {
   it('Then: I should be able to search books by title', async () => {
@@ -13,10 +14,10 @@ describe('When: Use the search feature', () => {
     await form.submit();
 
     const items = await $$('[data-testing="book-item"]');
-    expect(items.length).toBeGreaterThan(1);
+    expect(items.length).to.be.greaterThan(1, 'At least one book');
   });
 
-  xit('Then: I should see search results as I am typing', async () => {
+  it('Then: I should see search results as I am typing', async () => {
     await browser.get('/');
     await browser.wait(
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
--- libs/books/data-access/src/lib/+state/books.effects.ts
+++ libs/books/data-access/src/lib/+state/books.effects.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, debounceTime } from 'rxjs/operators';
 import { Book } from '@tmo/shared/models';
 import * as BooksActions from './books.actions';
+import { fetch } from '@nrwl/angular';
 
 @Injectable()
 export class BooksEffects {
   searchBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksActions.searchBooks),
-      switchMap((action) =>
-        this.http.get<Book[]>(`/api/books/search?q=${action.term}`).pipe(
-          map((data) => BooksActions.searchBooksSuccess({ books: data })),
-          catchError((error) => of(BooksActions.searchBooksFailure({ error })))
-        )
-      )
+      fetch({
+        run: action => {
+          return this.http
+            .get<Book[]>(`/api/books/search?q=${action.term}`)
+            .pipe(
+              map(data => BooksActions.searchBooksSuccess({ books: data }))
+            );
+        },
+
+        onError: (action, error) => {
+          console.error('Error', error);
+          return BooksActions.searchBooksFailure({ error });
+        }
+      })
     )
   );
 
